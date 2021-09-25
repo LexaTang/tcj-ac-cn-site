370a06e5c6d261276953ffdc79cb69a4b4e2526e
Lexa Tang <t@tcj.ac.cn>
2021-09-25 15:51:17 +0800

    feat: footer.

---
+++ src/Footer.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Footer(): React.ReactElement {
+  return (
+    <div className="footer">
+      <a href="https://beian.miit.gov.cn">赣ICP备2021007959号-1</a>
+    </div>
+  );
+}
