1110e41706d11a05ce53a99d35568a83387848ea
Lexa Tang <t@tcj.ac.cn>
2021-10-04 15:30:27 +0800

    feat: RUM.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import Home from './Home';
 import Footer from './Footer';
 
+// eslint-disable-next-line import/order
+import Aegis from 'aegis-web-sdk';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const aegis = new Aegis({
+  id: 'Z4vVGEA6o9pYNaY3Op', // 上报 id
+  uin: 'xxx', // 用户唯一 ID（可选）
+  reportApiSpeed: true, // 接口测速
+  reportAssetSpeed: true, // 静态资源测速
+  spa: true, // spa 应用页面跳转的时候开启 pv 计算
+});
+
 ReactDOM.render((
   <>
     <Home />
