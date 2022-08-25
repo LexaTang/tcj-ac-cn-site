7f0fb401a4f14a9839f361c71a90e857ab450a31
Alexandra Tang <t@tcj.ac.cn>
2022-08-25 13:20:25 +0800

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,8 +3,6 @@ import './Footer.css';
 
 export default function Footer(): React.ReactElement {
   return (
-    <div className="footer">
-      <a href="https://beian.miit.gov.cn" className="text-center">赣ICP备2021007959号-1</a>
-    </div>
+    <div className="footer" />
   );
 }
