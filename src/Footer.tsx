7e55b358cf4d12129f0ad86e7b3f4c1106a59fa0
Alexandra Tang <t@tcj.ac.cn>
2023-05-10 02:13:55 +0000

    Revert "Update Footer.tsx"
    This reverts commit 7f0fb401a4f14a9839f361c71a90e857ab450a31.

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,6 +3,8 @@ import './Footer.css';
 
 export default function Footer(): React.ReactElement {
   return (
-    <div className="footer" />
+    <div className="footer">
+      <a href="https://beian.miit.gov.cn" className="text-center">赣ICP备2021007959号-1</a>
+    </div>
   );
 }
